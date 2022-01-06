5efbdd6a5a9db42c9be5faa0d3f86e6f29e59c01
WangXiang <wx347249478@gmail.com>
2022-01-06 15:51:14 +0800

    add database parameter in matchDBTable function (#3)
    Signed-off-by: xiang <xiang13225080@163.com>

--- SQLHelper/src/main/java/org/chaos_mesh/byteman/helper/SQLParser.java
+++ SQLHelper/src/main/java/org/chaos_mesh/byteman/helper/SQLParser.java
@@ -27,26 +27,36 @@ public class SQLParser {
             Statement statement = CCJSqlParserUtil.parse(sql);
             TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
             switch(statement.getClass().getName()) {
+                case "org.chaosmesh.shaded.net.sf.jsqlparser.statement.select.Select":
                 case "net.sf.jsqlparser.statement.select.Select":
                     Select selectStatement = (Select)statement;
                     dbTableList = tablesNamesFinder.getTableList(selectStatement);
                     type = "select";
+                    break;
+                case "org.chaosmesh.shaded.net.sf.jsqlparser.statement.update.Update":
                 case "net.sf.jsqlparser.statement.update.Update":
                     Update updateStatement = (Update)statement;
                     dbTableList = tablesNamesFinder.getTableList(updateStatement);
                     type = "update";
+                    break;
+                case "org.chaosmesh.shaded.net.sf.jsqlparser.statement.insert.Insert":
                 case "net.sf.jsqlparser.statement.insert.Insert":
                     Insert insertStatement = (Insert)statement;
                     dbTableList = tablesNamesFinder.getTableList(insertStatement);
                     type = "insert";
+                    break;
+                case "org.chaosmesh.shaded.net.sf.jsqlparser.statement.replace.Replace":
                 case "net.sf.jsqlparser.statement.replace.Replace":
                     Replace replaceStatement = (Replace)statement;
                     dbTableList = tablesNamesFinder.getTableList(replaceStatement);
                     type = "replace";
+                    break;
+                case "org.chaosmesh.shaded.net.sf.jsqlparser.statement.delete.Delete":
                 case "net.sf.jsqlparser.statement.delete.Delete":
                     Delete deleteStatement = (Delete)statement;
                     dbTableList = tablesNamesFinder.getTableList(deleteStatement);
                     type = "delete";
+                    break;
                 // TODO: support more SQL type
                 default:
             }
@@ -71,8 +81,10 @@ public class SQLParser {
         return new SQLInfo(dbList, tableList, type);
     }
 
-    public static boolean matchDBTable(String sql, String filterDatabase, String filterTable, String sqlType) {
+    public static boolean matchDBTable(String database, String sql, String filterDatabase, String filterTable, String sqlType) {
+        System.out.println("database: " + database + ", sql: " + sql + ", filterDatabase: " + filterDatabase + ", filterTable: " + filterTable + ", sqlType: " + sqlType);
         SQLInfo sqlInfo = SQLParser.parseSQL(sql);
+        sqlInfo.dbList.add(database);
 
         if (sqlType != null && sqlType != "") {
             if (sqlInfo.type != sqlType) {
